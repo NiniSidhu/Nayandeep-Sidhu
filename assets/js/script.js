edaccba45e5154761cf39a1372ba6e4c744cf2c5
NayanSidhu <nsidh068@uottawa.ca>
2021-07-11 20:04:11 -0400

    added extras

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// var navi = document.querySelector('.navi');
+// var navigation = document.querySelector('.navigation')
+
+// navi.addEventListener('click', function(){
+//     console.log('This is working');
+
+
+// });
+
+const menuIcon = document.querySelector('.navi');
+
+
+
